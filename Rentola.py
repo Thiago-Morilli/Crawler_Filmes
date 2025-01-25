2dd14b312ffe95bf2196dfa6f2e99dab8b9a23c7
Thiago-Morilli <thiagomorilli50@gmail.com>
2025-01-25 14:32:32 +0000

    updated

--- Rentola.py
+++ Rentola.py
@@ -28,20 +28,17 @@ class Rentola:
 
     def extracting_data(self, soup):
 
-        test = soup.find("div", class_="mt-4 grid grid-flow-row grid-cols-1 gap-x-8 gap-y-4 xl:grid-cols-3").find_all("div", class_="flex justify-between rounded-xl border border-grey-200 bg-grey-100 p-4")
-        print(test)
         title = soup.find("h1", class_="text-[32px] font-bold").text
         information = soup.find("p", class_="line-clamp-5").text
-        type_property = soup.find("p", class_="text-sm font-[400]").text
+        type_property = soup.find("p", class_="text-sm font-[400]").text            
         price = soup.find("p", class_="mb-6 text-[32px] font-bold").text
+        entry_price = soup.find("span", class_="font-medium text-primary-100").text
+        if entry_price != "Ilimitado":
+            entry_price = entry_price
         rooms = soup.find_all("p", class_="text-sm font-[400]")[1].text
-        size = soup.find_all("p", class_="text-sm font-[400]")[2].text
-        meter_price = soup.find_all("p", class_="text-sm font-[400]")[7].text
-        if meter_price != "Sim":
-            meter_price = meter_price
-        #price_per_meter = soup.find("div", class_="flex justify-between rounded-xl border border-grey-200 bg-grey-100 p-4").find("p", class_="text-sm font-[400]")[8].text
-        washing_machine = soup.find()
+        location = soup.find("p", class_="text-primary-100").text
+        available = soup.find("span", class_="text-sm text-grey-400")
         
-        print()
+        
 
 Rentola().requisicao() 
